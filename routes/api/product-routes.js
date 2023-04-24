86b7cdea03914b63123a5fcfbd4d151e7aa4449b
TinaTheDev91 <122223572+TinaTheDev91@users.noreply.github.com>
2023-04-23 22:05:05 -0400

    created association relationships

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,12 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = Product.findAll();
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
