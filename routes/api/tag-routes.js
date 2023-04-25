f252b6b0b1a78e9bd016eb492654e1b7eaffdf97
TinaTheDev91 <122223572+TinaTheDev91@users.noreply.github.com>
2023-04-24 22:14:43 -0400

    finished tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -33,14 +33,53 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  try {
+    const tagData = Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  // update a tag's name by its `id` value\
+  Tag.update(
+    {
+      tag_name: req.body.tag_name
+    },
+    {
+      where: {
+        id: req.params.id
+      },
+    }
+  )
+    .then((updatedTag) => {
+      res.json(updatedtag);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id.'});
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
